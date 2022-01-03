de5776bc9bb8a42934e138c4d5b05e3bcc62482e
romaine877 <romaine877@gmail.com>
2022-01-03 02:42:09 -0500

    edit, delete and create authenticated endpoints adjusted

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -27,12 +27,18 @@ module.exports = {
 
     async create(ctx) {
         let entity;
+        const user = ctx.state.user
+
+        if(!user){
+            return ctx.badRequest(null, [{messages: [{id: "No Authorization Header found"}]}])
+        }
+    
         if (ctx.is('multipart')) {
           const { data, files } = parseMultipartData(ctx);
           data.user = ctx.state.user.id;
           entity = await strapi.services.events.create(data, { files });
         } else {
-          ctx.request.body.user = ctx.state.user.id;
+          ctx.request.body.user = user.id
           entity = await strapi.services.events.create(ctx.request.body);
         }
         return sanitizeEntity(entity, { model: strapi.models.events });
@@ -44,7 +50,7 @@ module.exports = {
         const user = ctx.state.user
 
         if(!user){
-            return ctx.badRequest(null, [{messages: [{id: "No Authorization Header found"}]}])
+            return ctx.badRequest(null, [{messages: [{id: "No Authorization Header found!!!!"}]}])
         }
     
         let entity;
@@ -71,6 +77,28 @@ module.exports = {
         return sanitizeEntity(entity, { model: strapi.models.events });
       },
     
+      //Delete event
 
+      async delete(ctx) {
+        const { id } = ctx.params;
+
+        const user = ctx.state.user
+
+        if(!user){
+            return ctx.badRequest(null, [{messages: [{id: "No Authorization Header found"}]}])
+        }
+    
+        const [events] = await strapi.services.events.find({
+          id: ctx.params.id,
+          "user.id": user.id,
+        });
+    
+        if (!events) {
+          return ctx.unauthorized(`You can't update this entry`);
+        }
+    
+        const entity = await strapi.services.events.delete({ id });
+        return sanitizeEntity(entity, { model: strapi.models.events });
+      },
       
 };
